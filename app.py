336504ca985d276474211853653a262a656233a3
Abiraame03 <abir44m3@gmail.com>
2026-01-30 20:16:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ RF_MODEL_PATH = "dyslexia_RF_model_mixed_chars_sentences_v3.joblib"
 DL_MODEL_PATH = "mobilenetv2_bilstm_final.h5"
 
 # Thresholds
-CANVAS_THRESHOLD = 0.545
+CANVAS_THRESHOLD = 0.549
 UPLOAD_THRESHOLD = 0.60
 
 IMG_SIZE_DL = (160, 160)
